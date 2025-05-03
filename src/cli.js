7d1ffa3a4c76e63602be5d974864a48769f6a2fa
al-gul <al-gul@rambler.ru>
2025-05-03 17:54:34 +0500

    update files

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const welcome = () => {
+const welcome = () => {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
 };
 
-export const yourAnswer = () => {
-  const evenAnswer = readlineSync.question('Your answer: ');
-  console.log(evenAnswer);
-};
+export default welcome;
